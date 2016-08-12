311561454114fda6221fbe055fcaf3ca99ccbf2c
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2016-08-12 15:13:16 +0200

    add browserify-zlib

--- main.js
+++ main.js
@@ -22,8 +22,6 @@ function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({width: 800, height: 600});
 
-
-
     // and load the index.html of the app.
     win.loadURL(`file://${__dirname}/index.html`);
 
