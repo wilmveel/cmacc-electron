bd45eae6b2a473c5a0cc3be77666f134546c6793
Axel Scheele <axel@Axels-MacBook-Pro.local>
2016-09-11 15:01:41 +0200

    added edit capabilities, copy, past, ect.

--- src/menu.js
+++ src/menu.js
@@ -107,6 +107,18 @@ module.exports = function(win, emitter) {
             ]
         },
         {
+            label:'Edit',
+            submenu:[
+                { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+                { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+                { type: "separator" },
+                { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+                { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+                { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+                { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+            ]
+        },
+        {
             label: 'IPFS',
             submenu: [
                 {
